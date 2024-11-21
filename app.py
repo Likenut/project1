c535b8dec85cce77e269c19e03795a7930335e5f
Magnus Prinz <magnus279r@gmail.com>
2024-11-21 15:55:00 +0100

    Added pages

--- app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Hello world")
-
-st.write("This is my first web app.")
---
+++ pages/Statistics.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Statistics",
+    page_icon="📊"
+)
+
+st.sidebar.success("Select page from above")
+
+st.title("Statistics")
