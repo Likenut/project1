b248797739b4cdd916ea0dfa5cb1dbc39d0ed68c
Magnus Prinz <magnus279r@gmail.com>
2024-11-21 15:58:09 +0100

    Added pages

---
+++ Homepage.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Homepage",
+    page_icon="🔭"
+)
+
+st.sidebar.success("Select page from above")
+
+st.title("Who's that scientist?")
+
+st.write("This is my first web app.")
